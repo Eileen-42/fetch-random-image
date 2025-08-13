c8269dd4b7ded180c13315166f3898c184337fb7
Eileen S <smileen.s@gmail.com>
2025-08-13 14:30:42 -0600

    semicolon missing

--- js/script.js
+++ js/script.js
@@ -7,5 +7,5 @@ const getImage = async function () {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
     console.log(images);
-}
+};
 getImage();
